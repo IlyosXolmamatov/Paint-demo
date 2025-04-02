4ccf95ac76bffee5b36cd6051963f1af5feaff98
IlyosXolmamatov <ilyosxolmamatov713@gmail.com>
2025-04-02 15:23:41 +0500

    Ranglar bilan ishlash funksiyasi qo'shildi

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@
 const canvas = document.querySelector("canvas"),
     toolBtns = document.querySelectorAll(".tool"),
     fillColor = document.querySelector("#fill-color"),
-    sizeSlider = document.querySelector("#size-slider")
+    sizeSlider = document.querySelector("#size-slider"),
+    colorBtns = document.querySelectorAll(".colors .option"),
+    colorPicker = document.querySelector("#color-picker")
+
 
 //Variables with value
 let ctx = canvas.getContext("2d"),
     isDrawing = false,
     brushWidth = 5,
     selectedTool = "brush",
+    selectedColor = "#000",
     prevMouseX,
     prevMouseY,
     snapshot
@@ -26,6 +30,8 @@ const startDrow = e => {
     prevMouseY = e.offsetY
     ctx.beginPath()
     ctx.lineWidth = brushWidth
+    ctx.strokeStyle = selectedColor
+    ctx.fillStyle = selectedColor
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
 }
 
@@ -84,14 +90,28 @@ toolBtns.forEach(btn => {
         document.querySelector(".options .active").classList.remove("active")
         btn.classList.add("active")
         selectedTool = btn.id
-        console.log(btn.id);
-
-
     })
 })
 
 //Change brush width
 sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value)
+
+//Set color to shapes
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        document.querySelector(".options .selected").classList.remove("selected")
+        btn.classList.add("selected")
+        const bgColor = window.getComputedStyle(btn).getPropertyValue("background-color")
+        selectedColor = bgColor
+    })
+})
+
+//Set  color  from color chicker
+colorPicker.addEventListener("change", e => {
+    colorPicker.parentElement.style.backgroundColor = colorPicker.value
+    colorPicker.parentElement.click()
+})
+
 
 //Stop drawing
 const stopDraw = () => {
